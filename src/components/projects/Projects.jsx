c1f0cddac6dd5f876fdaadc99a76662c95888230
priyanshsaxena24 <61087572+priyanshsaxena24@users.noreply.github.com>
2022-06-16 02:03:48 +0530

    worked on responsiveness

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FiGithub} from 'react-icons/fi'
+import { FiGithub } from 'react-icons/fi'
 import './projects.css'
 
 const Projects = () => {
@@ -9,54 +9,54 @@ const Projects = () => {
       <h2>Projects</h2>
       <h3 className='heading'>WEB DEVELOPMENT</h3>
       <div className='container project__container1'>
-        
+
         <div className='project__clothing'>
-        <a href='https://github.com/PriyaanshSaxena/Clothing-website'><FiGithub/></a>
+          <a href='https://github.com/PriyaanshSaxena/Clothing-website'><FiGithub /></a>
           <h2>Clothing Website</h2>
           <p>
-            An ecommerce clothing website made using ReactJS library having 
-            firebase authentication and integrated with stripe payments.   
+            An ecommerce clothing website made using ReactJS library having
+            firebase authentication and integrated with stripe payments.
           </p>
         </div>
-        
+
         <div className='project__disease'>
-        <a href='https://github.com/PriyaanshSaxena/Plant-Disease-Detection'><FiGithub/></a>
+          <a href='https://github.com/PriyaanshSaxena/Plant-Disease-Detection'><FiGithub /></a>
           <h2>Plant Disease Detection</h2>
           <p>
-          Plant Disease Detection is a project to detect the diseases in the plants by scanning the images of the leaves 
-          and then passing to through the neural network to detect wether the plant is infected or not.  
+            Plant Disease Detection is a project to detect the diseases in the plants by scanning the images of the leaves
+            and then passing to through the neural network to detect wether the plant is infected or not.
           </p>
         </div>
         <div className='project__scripto'>
-        <a href='https://github.com/PriyaanshSaxena/Scripto-react-project'><FiGithub/></a>
+          <a href='https://github.com/PriyaanshSaxena/Scripto-react-project'><FiGithub /></a>
           <h2>Scripto Website</h2>
           <p>
-           An online website where you can search for various scripts and ideas.  
+            An online website where you can search for various scripts and ideas.
           </p>
         </div>
       </div>
 
       <h3 className='heading'>ARTIFICIAL    INTELLIGENCE</h3>
       <div className='project__container2'>
-      <div className='project__assistant'>
-          <a href='https://github.com/PriyaanshSaxena/personalised-voice-assistant'><FiGithub/></a>
+        <div className='project__assistant'>
+          <a href='https://github.com/PriyaanshSaxena/personalised-voice-assistant'><FiGithub /></a>
           <h2>Personalised Voice Assistant</h2>
           <p>
             A Single Personalised Voice Assistant for Multiple users.
             Alexa listens to all and functions to their voice, but lacks the personalised results.
-            But this overcamed that issue. Accuracy : 98%. 
-            
+            But this overcamed that issue. Accuracy : 98%.
+
           </p>
         </div>
         <div className='project__malaria'>
-        <a href='https://github.com/PriyaanshSaxena/Malaria-detection-using-Deep-Learning'><FiGithub/></a>
+          <a href='https://github.com/PriyaanshSaxena/Malaria-detection-using-Deep-Learning'><FiGithub /></a>
           <h2>Malaria Detection using Deep Learning</h2>
           <p>
-          Detecting malaria for given images built with Tensorflow library using CNN having an accuracy of 97%.
+            Detecting malaria for given images built with Tensorflow library using CNN having an accuracy of 97%.
           </p>
         </div>
-        </div>
-      
+      </div>
+
     </section>
   )
 }
